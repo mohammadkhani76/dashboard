cb0a3cd3c165339e65de744de3d566d827f244ef
Mohammad Khani <nazanin.khani2@gmail.com>
2025-10-28 15:45:17 +0330

    added textLimited fun

--- src/pages/Dashboard/_components/DashboardBlog.jsx
+++ src/pages/Dashboard/_components/DashboardBlog.jsx
@@ -2,6 +2,14 @@ import ImgBlog from "../../../assets/pic/blog.png";
 import style from "./../Dashboard.module.css";
 
 export const DashboardBlog = () => {
+  const handelTextLimit = (text, maxlength) => {
+    if (!text) return "";
+    return text.length > maxlength ? text.slice(0, maxlength) + "..." : text;
+  };
+  const text = `آزمون‌های تمرینی ابزارهای ضروری برای تقویت دانش و بهبود مهارت‌های
+  آزمون‌دهی هستند. با شبیه‌سازی شرایط واقعی امتحان، این آزمون‌ها کمک
+  می‌کنند تا نقاط قوت و ضعف شناسایی شوند، اعتماد به نفس افزایش یابد
+  و اضطراب کاهش پیدا کند.`;
   return (
     <>
       <div className={style.dashLeft}>
@@ -9,12 +17,7 @@ export const DashboardBlog = () => {
           <img src={ImgBlog} alt="blog" />
           <div className={style.dashLeftItemDes}>
             <h4>آزمون‌های تمرینی: کلیدی برای موفقیت</h4>
-            <p>
-              آزمون‌های تمرینی ابزارهای ضروری برای تقویت دانش و بهبود مهارت‌های
-              آزمون‌دهی هستند. با شبیه‌سازی شرایط واقعی امتحان، این آزمون‌ها کمک
-              می‌کنند تا نقاط قوت و ضعف شناسایی شوند، اعتماد به نفس افزایش یابد
-              و اضطراب کاهش پیدا کند.
-            </p>
+            <p>{handelTextLimit(text, 81)}</p>
           </div>
         </div>
       </div>
